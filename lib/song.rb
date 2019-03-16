d9eefb33a6adf1e1e514b5e1838d3536a1e9fe94
Riccardo <riccardo_london@live.it>
2019-03-16 19:13:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ def self.find_by_name(tip)
 end
 
 def self.find_or_create_by_name(tip)
-  self.find_by_name(tip) != nil ? self : self.create_by_name(tip)
+  self.find_by_name(tip) != nil ?   self.find_by_name(tip) : self.create_by_name(tip)
 
 end
 

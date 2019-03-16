ce77824520ae75a403e5a310a00c6f1be47e355b
Riccardo <riccardo_london@live.it>
2019-03-16 19:12:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,123 +1,119 @@
-# require 'pry'
-#
-# class Song
-#
-#     attr_accessor :name, :artist_name
-#
-#     @@all = []
-#
-#     def initialize(name = nil)
-#       @name = name
-#       @@all << self
-#     end
-#
-#
-#     def self.all
-#       @@all
-#     end
-#
-#
-# def self.create
-#     song = self.new
-#     @@all << song
-#     song
-# end
-#
-# def self.new_by_name(name)
-#   song = self.new(name)
-#   song
-# end
-#
-# def self.create_by_name(name)
-#   song = self.new(name)
-#   @@all << song
-#   song
-# end
-#
-# def self.find_by_name(tip)
-#   @@all.select { |song| tip == song.name }.pop
-# end
-#
-# def self.find_or_create_by_name(tip)
-#   if (self.find_by_name(tip))
-#     return  self
-#   else
-#     self.create_by_name(tip)
-#   end
-#
-# end
-
-#
-# binding.pry
-# puts "SKSK"
-# end
-#
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 require 'pry'
 
 class Song
 
-  attr_accessor :name, :artist_name
+    attr_accessor :name, :artist_name
 
-  @@all = []
-  @@i = 1
-  def self.all
-    @@all
-  end
+    @@all = []
 
-  def save
-    self.class.all << self
-  end
+    def initialize(name = nil)
+      @name = name
+      @@all << self
+    end
 
-  def self.create
+
+    def self.all
+      @@all
+    end
+
+
+def self.create
     song = self.new
     @@all << song
-
     song
-  end
-
-  def self.new_by_name(title)
-   song = self.create
-   song.name = title
-   song
-  end
-
-  def self.create_by_name(name)
-    song = self.create
-    song.name = name
-    song
-  end
-
-  def self.find_by_name(search)
-    Song.all.select{ |i|  i.name == search  }.pop
-  end
-
-  def self.find_or_create_by_name(finder)
-    self.find_by_name(name) == finder ? self.create_by_name(finder) : false
-    self.find_by_name(name)
-  end
-
-def self.alphabetical
-  @@all
-  # binding.pry
-  # puts "VJNNF"
 end
 
+def self.new_by_name(name)
+  song = self.new(name)
+  song
 end
+
+def self.create_by_name(name)
+  song = self.new(name)
+  @@all << song
+  song
+end
+
+def self.find_by_name(tip)
+  @@all.select { |song| tip == song.name }.pop
+end
+
+def self.find_or_create_by_name(tip)
+  self.find_by_name(tip) != nil ? self : self.create_by_name(tip)
+
+end
+
+
+binding.pry
+puts "SKSK"
+end
+
+
+
+
+
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# require 'pry'
+# 
+# class Song
+# 
+#   attr_accessor :name, :artist_name
+# 
+#   @@all = []
+#   @@i = 1
+#   def self.all
+#     @@all
+#   end
+# 
+#   def save
+#     self.class.all << self
+#   end
+# 
+#   def self.create
+#     song = self.new
+#     @@all << song
+# 
+#     song
+#   end
+# 
+#   def self.new_by_name(title)
+#    song = self.create
+#    song.name = title
+#    song
+#   end
+# 
+#   def self.create_by_name(name)
+#     song = self.create
+#     song.name = name
+#     song
+#   end
+# 
+#   def self.find_by_name(search)
+#     Song.all.select{ |i|  i.name == search  }.pop
+#   end
+# 
+#   def self.find_or_create_by_name(finder)
+#     self.find_by_name(name) == finder ? self.create_by_name(finder) : false
+#     self.find_by_name(name)
+#   end
+# 
+# def self.alphabetical
+#   @@all
+#   # binding.pry
+#   # puts "VJNNF"
+# end
+# 
+# end
